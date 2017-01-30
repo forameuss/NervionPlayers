8c0b3d677296f08295b3f6260be978c0d9d3bdb0
adripol94 <apol@iesnervion.es>
2017-01-30 18:17:35 +0100

    Listado a la espera del Merge para mejora

---
+++ NervionPlayers_BL/src/NervionPlayers_BL/Listados/ListadosBL.cs
@@ -0,0 +1,20 @@
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace NervionPlayers_BL
+{
+    public class ListadosBL
+    {
+        public ListadosBL()
+        {
+            
+        }
+        public ObservableCollection<Alumno> listadoAlumnos() {
+                
+        }
+
+        public ObservableCollection<Equipo> listadoEquipos() {
+
+        }
+    }
+}

fd6d610ce0f02625e52ecacec5b8391f86cfd1db
Francisco Javier Ruiz Rodriguez <Pajarrurro@gmail.com>
2017-01-30 13:02:15 +0100

    prueba

---
+++ NervionPlayers_DAL/src/NervionPlayers_DAL/Listados/Prueba.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NervionPlayers_DAL.Listados
+{
+    public class Prueba
+    {
+    }
+}

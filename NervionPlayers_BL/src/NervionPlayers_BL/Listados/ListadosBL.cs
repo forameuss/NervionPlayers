78b0b8c7c7f7483a6a6c307acd0dc1666c7778ba
Adrian Pol <adripol1994@gmail.com>
2017-02-03 09:39:02 +0100

    Fin Listado cascara

--- NervionPlayers_BL/src/NervionPlayers_BL/Listados/ListadosBL.cs
+++ NervionPlayers_BL/src/NervionPlayers_BL/Listados/ListadosBL.cs
@@ -13,8 +13,76 @@ namespace NervionPlayers_BL
                 
         }
 
+        public ObservableCollection<Alumno> listadoAlumnos(int idAlumno)
+        {
+
+        }
+
         public ObservableCollection<Equipo> listadoEquipos() {
 
         }
+
+        public ObservableCollection<Equipo> listadoEquipos(int idEquipo)
+        {
+
+        }
+
+        public ObservableCollection<Partido> listadoPartidos()
+        {
+
+        }
+
+        public ObservableCollection<Partido> listadoPartidos(int idPartidos)
+        {
+
+        }
+
+        public ObservableCollection<Duelo> listadoDuelos()
+        {
+
+        }
+
+        public ObservableCollection<Duelo> listadoDuelos(int idDuelo)
+        {
+
+        }
+
+        public ObservableCollection<Deporte> listadoDeportes()
+        {
+
+        }
+
+        public ObservableCollection<Deporte> listadoDeportes(int idDeporte)
+        {
+
+        }
+
+        public ObservableCollection<AlumnoEquipo> listadoAlumnosEquipos()
+        {
+
+        }
+
+        public ObservableCollection<ResultadoPartido> listadoResultadoPartidos()
+        {
+
+        }
+
+        public ObservableCollection<ResultadoDuelo> listadoResultadoDuelos()
+        {
+
+        }
+
+        public ObservableCollection<Dispositivo> listadoDispositivos()
+        {
+
+        }
+
+        public ObservableCollection<Dispositivo> listadoDispositivos(int idAlumno)
+        {
+
+        }
+
+
+
     }
 }

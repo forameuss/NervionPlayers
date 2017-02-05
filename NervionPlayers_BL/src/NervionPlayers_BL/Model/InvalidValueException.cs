ed0230337033e88cba986f76a76c477aeb75c691
adripol94 <adripol94>
2017-02-05 21:00:42 +0100

    Añadido documentacion

--- NervionPlayers_BL/src/NervionPlayers_BL/Model/InvalidValueException.cs
+++ NervionPlayers_BL/src/NervionPlayers_BL/Model/InvalidValueException.cs
@@ -11,8 +11,24 @@ namespace NervionPlayers_BL.Model
     /// </summary>
     public class InvalidValueException : Exception
     {
+        /// <summary>
+        /// Excepcion que saltará al encontrar un valor invalido p que viole las condiciones propuestas.
+        /// </summary>
+        /// <param name="str">Informacion de la excepcion</param>
         public InvalidValueException(String str) { }
+
+        /// <summary>
+        /// Excepcion que saltará al encontrar un valor invalido p que viole las condiciones propuestas.
+        /// </summary>
+        /// <param name="str">Informacion de la excepcion</param>
+        /// <param name="line">Linea del error</param>
         public InvalidValueException(String str, int line) : base(str + " at line " + line) { }
+
+        /// <summary>
+        /// Excepcion que saltará al encontrar un valor invalido p que viole las condiciones propuestas.
+        /// </summary>
+        /// <param name="str">Informacion de la excpcion</param>
+        /// <param name="inner">Inner administrado</param>
         public InvalidValueException(String str, Exception inner) : base(str, inner) { }
     }
 }

1a5200f6bf537ee6801cb0cd69c848a16ec4b699
adripol94 <adripol94>
2017-02-05 22:23:14 +0100

    Medio hacer...

--- NervionPlayers_BL/src/NervionPlayers_BL/Manejadoras/ManejadoraDeporteBL.cs
+++ NervionPlayers_BL/src/NervionPlayers_BL/Manejadoras/ManejadoraDeporteBL.cs
@@ -1,4 +1,6 @@
-﻿using NervionPlayers_Ent.Modelos;
+﻿using NervionPlayers_BL.Model;
+using NervionPlayers_DAL.Manejadoras;
+using NervionPlayers_Ent.Modelos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +10,34 @@ namespace NervionPlayers_BL.Manejadoras
 {
     public class ManejadoraDeporteBL
     {
+        #region Var
+
+        private ManejadoraDeporteDAL manejadora;
+
+        #endregion
+
+        #region Constructores
+        /// <summary>
+        /// Instancia a <see cref="ManejadoraDeporteDAL"/>
+        /// </summary>
         public ManejadoraDeporteBL()
         {
-
+            manejadora = new ManejadoraDeporteDAL();
         }
+        #endregion
 
+        #region Metodos
+
+        /// <summary>
+        /// Obtiene un deporte a traves de <see cref="Alumno.Id"/>
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Objeto <see cref="Deporte"/> o Null</returns>
         public Deporte obtenerDeporte(int id)
         {
-            return null;
+            Deporte deporte = manejadora.obtenerDeporte(id);
+
+            return deporte;
         }
 
         public int insertarDeporte(int id)
@@ -32,5 +54,7 @@ namespace NervionPlayers_BL.Manejadoras
         {
             return 0;
         }
+
+        #endregion
     }
 }

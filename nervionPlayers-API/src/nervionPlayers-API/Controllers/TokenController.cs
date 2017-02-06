afee7a106a9df24c0e27d006672d22978c0f48ae
Francisco Javier Ruiz Rodriguez <Pajarrurro@gmail.com>
2017-02-06 10:17:22 +0100

    Inicio TokenController

---
+++ nervionPlayers-API/src/nervionPlayers-API/Controllers/TokenController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace nervionPlayers_API.Controllers
+{
+    [Route("api/Token")]
+    public class TokenController : Controller
+    {
+        // GET: /<controller>/
+        public void Index()
+        {
+            String auth = Request.Headers["Authorization"];
+            auth = Encoding.UTF8.GetString(Convert.FromBase64String(auth));
+            String[] miPersona = auth.Split(':');
+            
+        }
+    }
+}

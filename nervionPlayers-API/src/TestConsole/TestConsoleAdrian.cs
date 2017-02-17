99a3e69ea41374a2419f485b470e413cf64edb5a
Adrian Pol <adripol1994@gmail.com>
2017-02-17 10:20:02 +0100

    Agregada referencia en BL

--- nervionPlayers-API/src/TestConsole/TestConsoleAdrian.cs
+++ nervionPlayers-API/src/TestConsole/TestConsoleAdrian.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace TestConsole
 {
     public class TestConsoleAdrian
     {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+
     }
 }

2f5033b58afa966332316b1405d0f7a5862dbfea
Francisco Javier Ruiz Rodriguez <Pajarrurro@gmail.com>
2017-03-03 11:02:52 +0100

    Cambios menores en clases

---
+++ NervionPlayers_Ent/src/NervionPlayers_Ent/Modelos/AlumnoDeporte.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NervionPlayers_Ent.Modelos
+{
+    public class AlumnoDeporte
+    {
+        #region Atributos
+        private int id_Alumno;
+        private int id_Deporte;
+
+
+        #endregion
+
+        #region Constructores
+
+        public AlumnoDeporte()
+        {
+
+        }
+
+        public AlumnoDeporte(int id_alumno, int id_Deporte)
+        {
+            this.Id_Alumno = id_alumno;
+            this.id_Deporte = id_Deporte;
+        }
+
+        #endregion
+
+        #region Propiedades
+        public int Id_Alumno
+        {
+            get
+            {
+                return id_Alumno;
+            }
+
+            set
+            {
+                id_Alumno = value;
+            }
+        }
+
+        public int iId_Deporte
+        {
+            get
+            {
+                return id_Deporte;
+            }
+
+            set
+            {
+                id_Deporte = value;
+            }
+        }
+        
+        #endregion
+    }
+}
--- NervionPlayers_Ent/src/NervionPlayers_Ent/Modelos/ResultadoDuelo.cs
+++ NervionPlayers_Ent/src/NervionPlayers_Ent/Modelos/ResultadoDuelo.cs
@@ -11,7 +11,7 @@ namespace NervionPlayers_Ent.Modelos
     public class ResultadoDuelo
     {
         #region Atributos
-        private int id;
+        private int id_Deporte;
         private int id_Alumno;
         private int ganados;
         private int empatados;
@@ -22,9 +22,9 @@ namespace NervionPlayers_Ent.Modelos
         #region Constructores
         public ResultadoDuelo() { }
 
-        public ResultadoDuelo(int id, int id_Alumno, int ganados, int empatados, int perdidos)
+        public ResultadoDuelo(int id_Deporte, int id_Alumno, int ganados, int empatados, int perdidos)
         {
-            this.id = id;
+            this.id_Deporte = id_Deporte;
             this.id_Alumno = id_Alumno;
             this.ganados = ganados;
             this.empatados = empatados;
@@ -32,7 +32,7 @@ namespace NervionPlayers_Ent.Modelos
         }
         public ResultadoDuelo(ResultadoDuelo resultadoDuelo)
         {
-            this.id = resultadoDuelo.Id;
+            this.id_Deporte = resultadoDuelo.id_Deporte;
             this.id_Alumno = resultadoDuelo.Id_Alumno;
             this.ganados = resultadoDuelo.Ganados;
             this.empatados = resultadoDuelo.Empatados;
@@ -42,16 +42,16 @@ namespace NervionPlayers_Ent.Modelos
 
         #region Propiedades
 
-        public int Id
+        public int Id_Deporte
         {
             get
             {
-                return id;
+                return id_Deporte;
             }
 
             set
             {
-                id = value;
+                id_Deporte = value;
             }
         }
 
